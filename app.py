b765c46390bd776e64717b932bf78246535ae81c
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-04-20 18:30:26 +0000

    delete_ recipe function added and finshed edit_recipe

--- app.py
+++ app.py
@@ -62,6 +62,11 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('get_menus'))
 
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_menus'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '127.0.0.1'),
